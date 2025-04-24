4f073901587942b6edcf3ee583312e134bc979c0
Sidhi Garg <sidhigarg24@gmail.com>
2025-04-24 20:35:59 +0530

    Update server.js

--- XpressFleet/XpressFleet/backend/server.js
+++ XpressFleet/XpressFleet/backend/server.js
@@ -4,6 +4,8 @@ import { connectDB } from "./config/db.js";
 
 
 
+
+
 const serverStar = server.listen(process.env.PORT, async (err) => {
   if (err) {
     console.log(`server failed with error ${err}`);
