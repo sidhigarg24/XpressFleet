eae3ebdd963ad9a6fc2c4d917726346bf0a818bf
Sidhi Garg <sidhigarg24@gmail.com>
2025-04-24 20:28:35 +0530

    Update order.repository.js

--- XpressFleet/XpressFleet/backend/src/order/model/order.repository.js
+++ XpressFleet/XpressFleet/backend/src/order/model/order.repository.js
@@ -8,4 +8,28 @@ export const createNewOrderRepo = async (data) => {
   return newOrder;
 } catch (error) {
   console.log(error); 
+  throw error;
 }};
+
+export const findOrderByIdRepo = async (orderId) => {
+  return await OrderModel.findById(orderId).populate("user");
+};
+
+export const findOrdersByUserIdRepo = async (userId) => {
+  return await OrderModel.find({ user: userId });
+};
+
+export const findAllPlacedOrdersRepo = async () => {
+  return await OrderModel.find({}).populate("user");
+};
+
+export const updateOrderByIdRepo = async (orderId, updateData) => {
+  return await OrderModel.findByIdAndUpdate(orderId, updateData, {
+    new: true,
+    runValidators: true,
+  });
+};
+
+
+
+
