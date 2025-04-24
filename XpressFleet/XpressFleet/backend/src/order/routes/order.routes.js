bb540fc16d4bfabdd17c4c6a16d2ffbbc5d02bec
Sidhi Garg <sidhigarg24@gmail.com>
2025-04-24 20:29:40 +0530

    Update order.routes.js

--- XpressFleet/XpressFleet/backend/src/order/routes/order.routes.js
+++ XpressFleet/XpressFleet/backend/src/order/routes/order.routes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createNewOrder } from "../controllers/order.controller.js";
+import { createNewOrder, getSingleOrder, getMyOrders, getAllPlacedOrders, updateOrderDetails } from "../controllers/order.controller.js";
+
+
+
 import { auth } from "../../../middlewares/auth.js";
 
 const router = express.Router();
+router.route("/update/:id").put(auth, updateOrderDetails);
+router.route("/orders/placed").get(auth, getAllPlacedOrders);
+
+router.route("/my/orders").get(auth, getMyOrders);
 
 router.route("/new").post(auth, createNewOrder);
+// Route to get a single order by ID
+router.route("/:id").get(auth, getSingleOrder);
 
 export default router;
