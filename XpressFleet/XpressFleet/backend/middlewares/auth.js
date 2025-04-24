dd35363fd6c54c6f67a35adec3a19fd223d88499
Sidhi Garg <sidhigarg24@gmail.com>
2025-04-24 20:26:13 +0530

    Update auth.js

--- XpressFleet/XpressFleet/backend/middlewares/auth.js
+++ XpressFleet/XpressFleet/backend/middlewares/auth.js
@@ -4,13 +4,15 @@ import UserModel from "../src/user/models/user.schema.js";
 
 export const auth = async (req, res, next) => {
   const { token } = req.cookies;
+  console.log(token);
   if (!token) {
     return next(new ErrorHandler(401, "login to access this route!"));
   }
-  const decodedData = await jwt.verify(token, process.env.JWT_Secret);
+  const decodedData = await jwt.verify(token, process.env.JWT_SECRET);
   req.user = await UserModel.findById(decodedData.id);
   next();
 };
+
 
 export const authByUserRole = (...roles) => {
   return async (req, res, next) => {
